778c4cb30c2048a6bc19840627360379bae56525
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-04-15 12:48:15 +0900

    feat: successful /hello

--- handler/hello.go
+++ handler/hello.go
@@ -1,6 +1,11 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/projects/article-suggester/model"
+)
 
 type HelloHandler struct{}
 
@@ -9,4 +14,30 @@ func NewHelloHandler() *HelloHandler {
 }
 
 func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		var req model.Request
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if req.Message == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		res := &model.HelloResponse{
+			Message: "hello",
+		}
+		// if err != nil {
+		// 	w.WriteHeader(http.StatusInternalServerError)
+		// 	return
+		// }
+
+		if err := json.NewEncoder(w).Encode(&res); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
 }
--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"net/http"
-	"os"
 
 	"github.com/projects/article-suggester/handler/router"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	port := ":8080"
 	mux := router.NewRouter()
 	http.ListenAndServe(port, mux)
 }
