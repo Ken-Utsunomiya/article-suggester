6c47ac045c5c0e016c5a430ab5ef40dc0af39e0e
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-04-25 13:37:29 +0900

    feat: add auth slack middleware

--- src/handler/middleware/auth_slack.go
+++ src/handler/middleware/auth_slack.go
@@ -1,13 +1,30 @@
 package middleware
 
 import (
+	"io"
 	"net/http"
+	"os"
+
+	"github.com/slack-go/slack"
 )
 
 func AuthSlack(h http.Handler) http.Handler {
-	return http.HandlerFunc(authSlackFn)
-}
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
+		verifier, err := slack.NewSecretsVerifier(r.Header, signingSecret)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-func authSlackFn(w http.ResponseWriter, r *http.Request) {
-	// TODO: implement slack api authentication
+		r.Body = io.NopCloser(io.TeeReader(r.Body, &verifier))
+
+		if err = verifier.Ensure(); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
 }
